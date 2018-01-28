d996e19cc76479a37504b4264210aaf7f60824c3
sdvigov <sdvigov@gmail.com>
2018-01-28 22:07:53 +0300

    Initial Commit

---
+++ notif1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  notif1
+//
+//  Created by Mark Sdvigov on 28.01.18.
+//  Copyright © 2018 Mark Sdvigov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
